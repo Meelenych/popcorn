47967558e5f0a9d487fbc450f8ae762061b34a07
Meelenych <meelenych@gmail.com>
2024-11-07 12:55:26 -0700

    grid update

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -43,7 +43,7 @@ const MoviesList = () => {
 
 	return (
 		<div>
-			<ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 p-6 gap-4'>
+			<ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 p-6 gap-6'>
 				{loading ? (
 					<span className='loading loading-spinner text-accent'></span>
 				) : (
